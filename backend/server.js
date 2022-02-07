9cff2da735ce61b6e23f0a8a94fa9b0c5c2eccce
Soul Lee <lee.soul12@gmail.com>
2022-02-06 20:18:05 -0500

    reconfigure backend and finish setting up models, routes and controllers

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const routes = require('./routes/AppRouter');
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', routes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server Started On Port: ${PORT}`);
+});
